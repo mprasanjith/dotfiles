7cd9469625c440a3faf845b63e19ead3479c80f2
Madusha Prasanjith <madusha.prasanjith@pearson.com>
2019-03-23 14:18:47 +0530

    Fix permissions issues and additional tools.

--- bin/solus_third_party.sh
+++ bin/solus_third_party.sh
@@ -4,20 +4,16 @@ main() {
     cd /tmp
 
     # Install Chrome
-    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
-    sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+    sudo eopkg bi -y --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+    sudo eopkg it -y google-chrome-*.eopkg && sudo rm google-chrome-*.eopkg
 
     # Install Gitkraken
-    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/gitkraken/pspec.xml
-    sudo eopkg it gitkraken*.eopkg;sudo rm gitkraken*.eopkg
+    sudo eopkg bi -y --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/programming/gitkraken/pspec.xml
+    sudo eopkg it -y gitkraken*.eopkg && sudo rm gitkraken*.eopkg
 
     # Install Spotify
-    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/spotify/pspec.xml
-    sudo eopkg it spotify*.eopkg;sudo rm spotify*.eopkg
-
-    # Install WPS Office
-    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/wps-office/pspec.xml
-    sudo eopkg it wps-office*.eopkg;sudo rm wps-office*.eopkg
+    sudo eopkg bi -y --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/multimedia/music/spotify/pspec.xml
+    sudo eopkg it -y spotify*.eopkg && sudo rm spotify*.eopkg
 }
 
 main
